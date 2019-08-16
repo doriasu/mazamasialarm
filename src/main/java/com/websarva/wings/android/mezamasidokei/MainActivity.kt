6f8cf17ea4fcb71241f487f44a90fa9e3ce13013
TakuroOmori <omori.takuro@unipro.co.jp>
2019-08-16 14:16:36 +0900

    削除に対応した

--- src/main/java/com/websarva/wings/android/mezamasidokei/MainActivity.kt
+++ src/main/java/com/websarva/wings/android/mezamasidokei/MainActivity.kt
@@ -6,18 +6,25 @@ import android.view.View
 import android.app.PendingIntent
 import android.content.Context
 import android.app.AlarmManager
+import android.app.Dialog
 import android.content.BroadcastReceiver
+import android.content.DialogInterface
 import android.media.MediaPlayer
 import android.net.Uri
 import android.util.Log
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
 import java.io.IOException
 import java.lang.IllegalArgumentException
+import java.text.FieldPosition
 import java.text.SimpleDateFormat
 import java.util.*
 public var _player: MediaPlayer?=null
 public var adapter: ArrayAdapter<String>?=null
 public var count=0
+public var gohuncount=1
+
 class MainActivity : AppCompatActivity() {
 
 
@@ -55,6 +62,8 @@ class MainActivity : AppCompatActivity() {
             Log.e("MediaSample","メディアプレーヤー準備時の例外発生",ex)
 
         }
+        val lvMenu=findViewById<ListView>(R.id.list_view)
+        lvMenu.onItemClickListener=ListItemClickListener()
 
 
     }
@@ -67,10 +76,10 @@ class MainActivity : AppCompatActivity() {
 
     }
     //listviewのクリックに関する処理を記述
-    fun listclick(view :View){
 
-    }
     fun jikkouonclick(view : View){
+        //5分の方を初期化
+        gohuncount=1
         var calendar = jikokushutoku()
 
         val alarmTime=calendar.timeInMillis
@@ -80,7 +89,33 @@ class MainActivity : AppCompatActivity() {
         //Intent作成
         val alarmIntent= Intent(this,AlarmReceiver::class.java)
 
-        var moji=calendar.get(Calendar.HOUR).toString()+"時"+calendar.get(Calendar.MINUTE).toString()+"分"
+        var moji=count.toString()+":"+calendar.get(Calendar.HOUR).toString()+"時"+calendar.get(Calendar.MINUTE).toString()+"分"
+        adapter?.add(moji)
+        //以下時間指定操作
+
+
+        val pendingintent=PendingIntent.getBroadcast(this,count,alarmIntent,PendingIntent.FLAG_UPDATE_CURRENT)
+        count++
+
+        //alarmmanagerで指定時間後に処理を設定
+        val manager=getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        manager.set(AlarmManager.RTC_WAKEUP,alarmTime,pendingintent)
+        manager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,alarmTime,pendingintent)
+
+    }
+
+    fun gohunonclick(view :View){
+
+        var calendar = jikokushutokugohun()
+
+        val alarmTime=calendar.timeInMillis
+        //val alarmTime = System.currentTimeMillis() + 5000
+
+        //Toast.makeText(this,"${alarmTime}",Toast.LENGTH_LONG).show()
+        //Intent作成
+        val alarmIntent= Intent(this,AlarmReceiver::class.java)
+
+        var moji=count.toString()+":"+calendar.get(Calendar.HOUR).toString()+"時"+calendar.get(Calendar.MINUTE).toString()+"分"
         adapter?.add(moji)
         //以下時間指定操作
 
@@ -121,10 +156,56 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+    fun jikokushutokugohun() :Calendar{
+        val settime=findViewById<TimePicker>(R.id.time_picker)
+        val hour=settime.getHour()
+
+        val minute=settime.getMinute()
+        val today=getToday()
+        //calendarにのせる
+        var calendar=Calendar.getInstance()
+        //javaの月は0indexに注意
+        calendar.set(today.substring(0,4).toInt(),today.substring(5,6).toInt()-1,today.substring(6,8).toInt(),hour,minute+ gohuncount*5,0)
+        gohuncount++
+
+
+        return calendar
+
+
+
+
+    }
+
     fun getToday(): String {
         val date = Date()
         val format = SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault())
         return format.format(date)
+    }
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener{
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            //タップされた名前の取得
+            val item=parent?.getItemAtPosition(position) as String
+            //intent発行して削除
+            val alarmIntent= Intent(applicationContext,AlarmReceiver::class.java)
+            //9コマでしか無理そう
+            val pendingintent=PendingIntent.getBroadcast(applicationContext,(item.substring(0,1)).toInt(),alarmIntent,PendingIntent.FLAG_UPDATE_CURRENT)
+            pendingintent.cancel()
+            val manager=getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            manager.cancel(pendingintent)
+            adapter?.remove(adapter?.getItem(position))
+
+
+
+
+
+
+
+
+
+
+        }
     }
 
 }
@@ -149,4 +230,5 @@ class AlarmReceiver : BroadcastReceiver(){
     }
 
 }
+
 
